ef9f0bef2979f34f17304a9a2e61849d6e04500d
justin_james <jjames11799@gmail.com>
2020-07-05 21:13:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,35 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: :any_blank?
+  
+  def any_blank?(attributes)
+     attributes['name'].empty? || attributes['user_id'].empty?
+  end
 
+
+  def category_ids=(ids)
+    ids.each do |id|
+      unless id.empty? || id.nil?
+      category = Category.find(id) unless id == 0
+       self.categories << category
+      end
+    end
+  end
+
+  # def cagtegory_ids
+  #   self.categories.each do |category|
+  #     category = Category.id 
+      
+  # end
+
+  def categories_attributes=(category_attributes)
+    # byebug
+    category_attributes.values.each do |category_attribute|
+      unless category_attribute['name'].empty?
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category 
+      end 
+    end
+  end
 end
